1ee41ae54d9048c52b46ec0e97bc6c8ae73c96e9
abhinavkr2108 <abhinavkr.2108@gmail.com>
2025-07-09 22:06:42 +0530

    Intialized shadcnui

--- frontend/src/pages/home/page.tsx
+++ frontend/src/pages/home/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "../../components/ui/button";
+
 export default function HomePage() {
-  return <div className="font-bold text-green-500">Home page</div>;
+  return (
+    <div className="font-bold text-green-500">
+      Home page
+      <Button>Click me</Button>
+    </div>
+  );
 }
