de551e2facaa763c40c28aa4c27b230ed32fdce6
abhinavkr2108 <abhinavkr.2108@gmail.com>
2025-07-10 19:29:28 +0530

    Logo title change

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -59,7 +59,7 @@ export default function Header({
     url: "/",
     src: "/logo.png",
     alt: "logo",
-    title: "GVibrationBusters",
+    title: "gVibrationbusters",
   },
   menu = [
     { title: "Home", url: "/" },
